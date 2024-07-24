56a2437dd2bc62c39291e566ececc3068140c86c
chrisbmx20 <calvarez@fwdcostarica.com>
2024-07-24 13:47:35 -0600

    proyecto-js

--- js/functions.js
+++ js/functions.js
@@ -1,4 +1,4 @@
-function showElement(){
+function showElement(index){
     let event = document.getElementById("events");
     let task = document.getElementById("tasks");
 
@@ -18,15 +18,26 @@ let eventList = document.getElementById("event-list");
 let addEventBtn = document.getElementById("addEvent");
 
 
-addEventBtn.addEventListener("click",function(){
+getEvents();
 
+addEventBtn.addEventListener("click",function(){
+/*
     let eventItem = document.createElement("li");
     let eventHeading = document.createElement("h3");
-    let evntDate = document.createElement("span");
+    let evntDate = document.createElement("span");*/
         
-    let eventTitle = document.getElementById("eventTitle");
-    let eventDate = document.getElementById("eventDate");
+    let eventTitle = document.getElementById("eventTitle").value;
+    let eventDate = document.getElementById("eventDate").value;
 
+    if (eventTitle !="" && eventDate!=""){
+        addEvent(eventTitle,eventDate);
+        eventTitle ="";
+        saveEvents();
+
+    }
+
+    
+/*
     let deleteBtn = document.createElement("button");
         deleteBtn.innerHTML = "Delete";
     let editBtn = document.createElement("button");
@@ -43,6 +54,7 @@ addEventBtn.addEventListener("click",function(){
         eventItem.childNodes[1].textContent = prompt("Edit Event Date",eventItem.childNodes[1].textContent);
     })
 
+
     eventHeading.innerHTML = eventTitle.value;
     evntDate.innerHTML = eventDate.value;
 
@@ -52,9 +64,44 @@ addEventBtn.addEventListener("click",function(){
     eventItem.appendChild(editBtn);
 
     eventList.appendChild(eventItem);
+    */
 }
 );
 
+function addEvent(evtitle,evDate){
+    let eventItem = document.createElement("li");
+    let eventHeading = document.createElement("h3");
+    let evntDate = document.createElement("span");
+        
+    eventHeading.innerHTML = evtitle;
+    evntDate.innerHTML = evDate;
+
+    let deleteBtn = document.createElement("button");
+        deleteBtn.innerHTML = "Delete";
+    let editBtn = document.createElement("button");
+        editBtn.innerHTML = "Edit";
+
+    //Eliminar Item de Lista
+    deleteBtn.addEventListener("click", function(){
+        eventList.removeChild(eventItem);
+        saveEvents();
+    });
+
+    //Editar Item de Lista
+    editBtn.addEventListener("click", function(){
+        eventItem.firstChild.textContent = prompt("Edit Your Event",eventItem.firstChild.textContent);
+        eventItem.childNodes[1].textContent = prompt("Edit Event Date",eventItem.childNodes[1].textContent);
+        saveEvents();
+    })
+
+    eventItem.appendChild(eventHeading);
+    eventItem.appendChild(evntDate);
+    eventItem.appendChild(deleteBtn);
+    eventItem.appendChild(editBtn);
+
+    eventList.appendChild(eventItem);
+
+}
 
 function saveEvents() {
     const eventArr = [];
@@ -69,6 +116,8 @@ function saveEvents() {
         event.date =  eventItem.childNodes[1].textContent;
         eventArr.push(event);
 
+        console.log("evento ", event);
+
     });
 
     localStorage.setItem('events', JSON.stringify(eventArr));
@@ -77,7 +126,7 @@ function saveEvents() {
 
 function getEvents() {
     const events = JSON.parse(localStorage.getItem('events')) || [];
-    events.forEach(event => addEvent(event));
+    events.forEach(event => addEvent(event.title,event.date));
     
 }
 
